10b1bbb42d4c310f6f6ab8b270595154a52b4b5b
Evgeniy <bev.21@uni-dubna.ru>
2022-04-03 13:48:45 +0300

    Переделанное дз_1

--- Bodrov/my_homework_1.py
+++ Bodrov/my_homework_1.py
@@ -1,106 +1,82 @@
 from re import X
 
+def reversList(list):
+    l = []
+    b=len(list)
+    for i in range(b):
+        l.append(list[b-i-1])
+    return l
+  
 
-Ishodniy_spisok=[1,"ZERO",354,"What do you do?", 156]
-End=[]
-def revers(Ishodniy_spisok, End):
- for n in range(len(Ishodniy_spisok)):
-    End[n]=Ishodniy_spisok[len(Ishodniy_spisok)-n - 1]
+#-----------------------------------
 
-    return(End)
+def Sred(Spisok):
+    sum = 0 
+    b = len(Spisok)
+    for n in range(b):
+        sum = sum + len(Spisok[n])
 
+    return (sum/b)
 
-#------------------------------------------------
-
-Spisok=[12, 378, 4]
-sum=0
-def Sred(sum, Spisok):
- for n in range(len(Spisok)):
-    sum=sum+len(Spisok[n])
-
-    return(sum/len(Spisok))
-
-#------------------------------------------------
-arg = ["her", "name", "is", "Masha", "Masha", "is", "a", "sister", "of", "Zhenya"]
-def Slovar(arg):
-    for n in range(len(arg)):
-        return(arg[n]+n)
 
 #------------------------------------------------
 
-arg1 = ["my", "name", "is", "Masha"]
-arg2 = ["my", "name", "is", "Zhenya"]
-i=0
-ARG=[]
-A=0
+def ToDict(list):
+    res = {}
+
+    for i in range(0, len(list)):
+        if list[i] in res:
+            if type(res[list[i]]) != list:
+                a = res[list[i]]
+                res[list[i]] = [a, i]
+            else:
+                res[list[i]].append(i)
+            continue
+        res[list[i]] = i
+    return res
+#------------------------------------------------
+
 def Sovpad(arg1, arg2):
-    for n in range(len(arg1)):
-        if arg1[n]==arg[i]:
-            ARG[A]=arg1[n]
-            A=A+1
-    i=i+1
-    
-    return ARG
+    A=set(arg1)
+    B=set(arg2)
+    C = []
+    C= A & B
+    return C
 
 #------------------------------------------------
 
-arg = ["aaa", "aaa", "bbb", "ccc", "bbb"]
-i=0
-N=1
+
 def Chastota(arg):
-    for i in range(len(arg)):
-        for n in range(len(arg)):
+    C = {}
+    b=len(arg)
+    for i in range(b):
+        N=0
+        for n in range(b):
             if arg[i]==arg[n]:
                 N=N+1
                 continue
-            if n==len(arg)-1:
-                return(arg[i]+N)
+            if n==b-1:
+                C[i] = N
+    return C
+
 
 #--------------------------------------------
 
-arg = ["abcd", "a", "ab", "abc", "bazinga", "bar"]
-max=1000
-arg2=[]
 def sort(arg):
-    for i in range(len(arg)):
-        for n in range(len(arg)):
-            if arg[n]<=min:
-                b=n
-                min=arg[n]
-            
-            if n==len(arg)-1:
-                c=arg[b]
-                arg[b]=arg[i]
-                arg[i]=c
-                n=n+1  
-
+    C = []
+    d=len(arg)
+    for j in range(d):
+        min=100
+        for i in range(d):
+            if len(arg[i]) < min:
+                min = len(arg[i])
+                n=i
+        C.append(arg[n])
+        del arg[n]
+        d=d-1
+    return C
 #-----------------------------------------------------
-B0=1
-N0=2
-B1=2
-N1=3
-F=[B0,N0]
-L=[B1, N1]
 
-def Chess(F, L):
-    for i in range(9):
-        if F[0]==L[0] or F[1]==L[1]:
-          print('true')
-          
-          if F[0]==L[0]-1*i and F[1]==L[1]-1*i:
-              print ('true')
-
-              if F[0]==L[0]+1*i and F[1]==L[1]+1*i:
-                  print ('true')
-                  
-                  if F[0]==L[0]+1*i and F[1]==L[1]-1*i:
-                    print ('true')
-
-                  if F[0]==L[0]-1*i and F[1]==L[1]+1*i:
-                     print ('true')
-                     
-        else:
-            print("false")
 
 
 
