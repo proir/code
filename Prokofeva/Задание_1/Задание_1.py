7ca6421b59a40c161695dd1cfb1328769edcb5b7
proir <Aprokoeva1@mail.ru>
2022-05-22 21:54:13 +0300

    Задание №1

---
+++ Prokofeva/Задание_1/Задание_1.py
@@ -0,0 +1,17 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name="spidercode",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Spider code",
+    author="Peter Parker",
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
+
